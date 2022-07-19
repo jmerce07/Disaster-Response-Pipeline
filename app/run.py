2c0952eab48e67c61d019cb2e9d93d0e7290f20b
Jeremy Mercer <72773273+jmerce07@users.noreply.github.com>
2022-07-18 23:03:01 -0400

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,14 +1,16 @@
 import json
 import plotly
 import pandas as pd
+import re
 
+from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -27,7 +29,7 @@ def tokenize(text):
 
 
 # load data
-engine = create_engine('sqlite:///DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql('DisasterResponse', engine)
 
 # load model
@@ -42,7 +44,51 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    
+    df1 = df.drop(['id','message','original','genre'], axis=1)
+    category_counts=df1.sum(axis=0)
+    category_names = df1.columns
+    
+    # Transpose the data to a wide format
+    df2= df.transpose()
+    
+    # Combine all of the messages into one string of all the messages
+    df3 = df2[df2.columns[0:]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
+    text = df3.iloc[1]
+    
+    # Remove punctuation characters and convert to lowercase
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
 
+    #Split text into words using NLTK
+    words = word_tokenize(text)
+
+    # Remove stop words
+    words = [w for w in words if w not in stopwords.words("english")]
+    
+    # Join all of the words together to get a string instead of a list
+    text2 = ' '.join(words)
+    
+    def word_count(str):
+        counts = dict()
+        words = str.split()
+
+        for word in words:
+            if word in counts:
+                counts[word] += 1
+            else:
+                counts[word] = 1
+
+        return counts
+
+    word_count(text2)
+    
+    text_df = pd.DataFrame.from_dict(word_count(text2),orient='index')
+    text_df.index.name="Word"
+    text_df.reset_index(inplace=True)
+    text_df.columns.values[1] = "Count"
+    text_df.sort_values(by='Count', ascending=False, inplace=True)
+    text_df2 = text_df.head(10)
+     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -63,6 +109,42 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=text_df2["Word"],
+                    y=text_df2["Count"]
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 10 Used Words In The Message Data',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Word"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
         }
     ]
 
@@ -73,6 +155,8 @@ def index():
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
+
+   
 
 # web page that handles user query and displays model results
 @app.route('/go')
